4619f8343fabde88bddae513653dfbb3422ea471
Tolkunio <tolkun@opencbs.com>
2018-06-21 21:49:34 +0600

    -add data acces layer and code first migration

---
+++ FriendOrganizer.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FriendOrganizerDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FriendOrganizerDbContext context)
+        {
+            context.Friends.AddOrUpdate(f=>f.FirstName,
+                new Friend{FirstName = "Thomas", LastName = "Huber"},
+                new Friend{FirstName = "Urs",LastName = "Meier"},
+                new Friend{FirstName = "Erkan",LastName = "Egin"});
+        }
+    }
+}
